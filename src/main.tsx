bb3d9bb65f0e8fc1ec505c2f4c585da4ae43a482
Natthaphon-SNT <zfrankenz123@gmail.com>
2026-09-01 14:16:10 +0700

    Last

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { loader } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
 import App from "./App";
 import "./index.css";
+
+// Configure @monaco-editor/react to use local bundled monaco instance
+// instead of trying to fetch from external CDN (jsdelivr), which gets blocked by CSP
+loader.config({ monaco });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
